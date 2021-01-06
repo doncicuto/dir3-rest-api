8efdf95cd3d470ebb18ff187ca7d432568846e3d
Miguel Cabrerizo <doncicuto@gmail.com>
2021-01-06 18:08:57 +0100

    fix(ci): reduce CPU limits for replica sets

--- src/swagger-spec.ts
+++ src/swagger-spec.ts
@@ -7,7 +7,7 @@ const options = {
       title: "DIR3 Rest API",
       version: "1.0.0",
       description:
-        "A REST API to query DIR3 identification codes, as well as other information, of the different Spanish public administrations obtained from publicly available files in the Download Area of ​​the Technology Transfer Center of the Electronic Administration Portal (CTT). The url of the download area is https://administracionelectronica.gob.es/ctt/dir3/descargas",
+        "A REST API to query DIR3 id codes, as well as other information, of the different Spanish public administrations obtained from publicly available files in the Download Area of ​​the Technology Transfer Center of the Electronic Administration Portal (CTT). The url of the download area is https://administracionelectronica.gob.es/ctt/dir3/descargas",
       license: {
         name: "MIT",
         url: "https://choosealicense.com/licenses/mit/",
